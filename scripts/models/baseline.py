3f246e6f3cb6e7f980e3f1435c8707f8c90a18e7
erasromani <ebrahim.rasromani@gmail.com>
2022-04-20 15:26:18 -0400

    implementing clinicallongformer mode

---
+++ scripts/models/baseline.py
@@ -0,0 +1,46 @@
+from torch import nn
+from transformers import AutoModel
+
+
+def resolve_feature_extractor(name):
+  if name == "clinicalbiobert":
+    return AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+  elif name == "clinicallongformer":
+    return AutoModel.from_pretrained("yikuan8/Clinical-Longformer")
+  else:
+      ValueError(f"invalid feature extractor name {name}")
+
+
+class ClinicalBioBERT(nn.Module):
+  def __init__(self, num_classes):
+      super(ClinicalBioBERT, self).__init__()
+      self.feature_extractor = resolve_feature_extractor("clinicalbiobert")
+      self.classifier = nn.Linear(self.feature_extractor.get_input_embeddings().embedding_dim, num_classes)
+      self.loss_func = nn.CrossEntropyLoss()
+  
+  def forward(self, **kwargs):
+    x = self.feature_extractor(input_ids=kwargs['input_ids'], attention_mask=kwargs['attention_mask'])
+    logits = self.classifier(x['pooler_output'])
+    label = kwargs['labels']
+    loss = self.loss_func(logits, label)
+    return {
+      "loss": loss,
+      "logits": logits
+    }
+
+class ClinicalLongformer(nn.Module):
+  def __init__(self, num_classes):
+      super(ClinicalLongformer, self).__init__()
+      self.feature_extractor = resolve_feature_extractor("clinicallongformer")
+      self.classifier = nn.Linear(self.feature_extractor.get_input_embeddings().embedding_dim, num_classes)
+      self.loss_func = nn.CrossEntropyLoss()
+  
+  def forward(self, **kwargs):
+    x = self.feature_extractor(input_ids=kwargs['input_ids'], attention_mask=kwargs['attention_mask'])
+    logits = self.classifier(x['pooler_output'])
+    label = kwargs['labels']
+    loss = self.loss_func(logits, label)
+    return {
+      "loss": loss,
+      "logits": logits
+    }
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import torch
+import datetime
 
 def epoch_iter(num_epochs, dataloader):
     steps_per_epoch = len(dataloader)
@@ -18,3 +19,10 @@ def save_weights(model, optimizer, filename, **kwargs):
     for key, val in kwargs.items():
       state[key] = val
     torch.save(state, filename)
+
+def get_save_path(output_dir):
+  timestamp = datetime.datetime.now()
+  date = timestamp.strftime("%Y%m%d")
+  time = timestamp.strftime("%H%M%S")
+  save_path = f'{output_dir}/{date}/{time}'
+  return save_path
